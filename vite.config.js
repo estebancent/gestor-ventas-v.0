9602f8e67b99a60960a5a1bcb4bd9a8bea44c72f
esteban.cent95@gmail.com <esteban.cent95@gmail.com>
2026-03-17 12:00:33 -0300

    editando

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'; // Si usas Tailwind 4
+import path from 'path'; // Agrega esto al principio
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-})
+    resolve: {
+        alias: {
+            // Esto le dice a Vite que '../../vendor' es la raíz del proyecto
+            '../../vendor': path.resolve(__dirname, './vendor'),
+            'vendor': path.resolve(__dirname, './vendor'),
+        },
+    },
+});
